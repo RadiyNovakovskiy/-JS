cc1afd723b23c856342f9fa91279544b4d5a15f1
Radiy <radya1024@gmail.com>
2021-09-21 16:08:34 +0300

    Практика JS 4. Дан массив ['Саша', 'Петя', 'Оксана'], сделайте из него ['Оксанa', 'Олег', 'Саша']. Реализуйте эту задачу, двумя способами. В первом исходный массив должен быть изменен, во втором - не должен быть изменен.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 "use strict"
 
-// Практика JS 3
+// Практика JS 4
 
-let name = prompt("Введите ваше имя: ");
+let array = ["Саша", "Петя", "Оксана"];
 
-alert("Ваше имя: " + name);
+console.log("Исходный массив: " + array);
+
+let reverse_array = array.slice().reverse();
+reverse_array.splice(1,1,"Олег");
+
+console.log("Измененный массив: " + reverse_array);
+console.log("Исходный массив: " + array);
+
+array[0] = "Оксана";
+array[1] = "Олег";
+array[2] = "Саша";
+
+console.log("Измененный массив: " + array);
