488d83b5d665ae81d3d39f7d2b3fc78901a83b70
Radiy <radya1024@gmail.com>
2021-09-21 15:20:15 +0300

    Практика JS 1. Создайте переменную с помощью camelCase, snake_case и PascalCase и присвойте им значения от 0-я до 2-х. ыведите значения этих переменных на экран с помощью метода alert и console.log.

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// Практика JS 1
+
+let camelCase = 0;
+let snake_case = 1;
+let PascalCase = 2;
+
+console.log("camelCase: " + camelCase);
+console.log("snake_case: " + snake_case);
+console.log("PascalCase: " + PascalCase);
+
+alert("camelCase: " + camelCase);
+alert("snake_case: " + snake_case);
+alert("PascalCase: " + PascalCase);
+
+// alert выводит переменную в модальное окно в браузере 
+// console.log выводит переменную в консоль браузера
