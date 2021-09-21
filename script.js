068189083dba5ac33a8f3edd6ae9798df59536aa
Radiy <radya1024@gmail.com>
2021-09-21 15:45:51 +0300

    Практика JS 2. Дана строка str = 'HTML'. Примените к ней определенный метод строк, чтобы все буквы стали маленькими. Выведите результат в консоль.

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 "use strict"
 
-// Практика JS 1
+// Практика JS 2
 
-let camelCase = 0;
-let snake_case = 1;
-let PascalCase = 2;
+let str = "HTML";
 
-console.log("camelCase: " + camelCase);
-console.log("snake_case: " + snake_case);
-console.log("PascalCase: " + PascalCase);
-
-alert("camelCase: " + camelCase);
-alert("snake_case: " + snake_case);
-alert("PascalCase: " + PascalCase);
-
-// alert выводит переменную в модальное окно в браузере 
-// console.log выводит переменную в консоль браузера
+console.log(str.toLowerCase());
