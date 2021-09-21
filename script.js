a1bc160c39ce69e8782beba71040ec84960e294c
Radiy <radya1024@gmail.com>
2021-09-21 19:35:40 +0300

    Практика JS 21. Дана строка, состоящяя из букв a,b,c. Ваша задача поменять расположение букв a и b, но не трогать c.

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 "use strict"
 
-// Практика JS 20
+// Практика JS 21
 
-let types = new function(a){
-    this.boolean = new Array();
-    this.number = new Array();
-    this.string = new Array();
-    a = [1, 2, 'a', 'b', true, false];
-    a.forEach(item => {
-        if(typeof(item) === "boolean"){
-            this.boolean.push(item);
-        }
-        else if(typeof(item) === "number"){
-            this.number.push(item);
-        }
-        else if (typeof(item) === "string"){
-            this.string.push(item);
-        }
-    });
-}
-console.log(types);
+const replaceSymbol = (string) => string.replace(/[ab]/g, function($1) { return $1 === 'a' ? 'b' : 'a' });
+
+alert(replaceSymbol('abc'));
+alert(replaceSymbol('aaabcccbaaa'));
+alert(replaceSymbol('aabacbaa'));
+alert(replaceSymbol('ccccc'));
