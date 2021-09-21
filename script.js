dc33b75954c6af12f142c7f0764212a86ce8189d
Radiy <radya1024@gmail.com>
2021-09-21 18:06:29 +0300

    Практика JS 9. Напишите функцию, которая принимает в себя массив, а возвращает произведение элементов массива, умноженных по порядку друг на друга.

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 "use strict"
 
-// Практика JS 8
+// Практика JS 9
 
-function finalGrade(examMark, projectCount){
-    let mark = 0;
-    if(examMark > 90 || projectCount > 10){
-        mark = 100;
-    }
-    else if(examMark > 75 || projectCount > 5){
-        mark = 90;
-    }
-    else if(examMark > 50 || projectCount > 2){
-        mark = 75;
-    }
-    else{
-        mark = 0;
-    }
-    return mark;
-}
+const returnMultiplication = (array) => array.reduce((a, b) => a*b);
 
-alert("Ваша оценка: " + finalGrade(100, 12));
-alert("Ваша оценка: " + finalGrade(99, 0));
-alert("Ваша оценка: " + finalGrade(10, 15));
-alert("Ваша оценка: " + finalGrade(85, 5));
-alert("Ваша оценка: " + finalGrade(55, 3));
-alert("Ваша оценка: " + finalGrade(55, 0));
-alert("Ваша оценка: " + finalGrade(20, 2));
+console.log((returnMultiplication([1, 2, 3, 4])));
